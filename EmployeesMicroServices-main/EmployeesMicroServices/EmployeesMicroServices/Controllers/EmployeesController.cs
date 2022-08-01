e7e64b0d66f61f42737c63b19bfafa2fe6f47dcb
mukeshkannan007 <79103767+mukeshkannan007@users.noreply.github.com>
2022-08-01 17:54:48 +0530

    Add files via upload

---
+++ EmployeesMicroServices-main/EmployeesMicroServices/EmployeesMicroServices/Controllers/EmployeesController.cs
@@ -0,0 +1,35 @@
+﻿using EmployeesMicroServices.Models;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EmployeesMicroServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("AllowOrigin")]
+    public class EmployeesController : ControllerBase
+    {
+        static EmployeeBO context = new EmployeeBO();
+        // GET: api/<EmployeesController>
+        [HttpGet]
+        public IEnumerable<EmployeeModel> Get() => context.GetAllEmployees();
+
+        // GET api/<EmployeesController>/5
+        [HttpGet("{id}")]
+        public EmployeeModel Get(int id) => context.GetEmployeeByid(id);
+
+        // POST api/<EmployeesController>
+        [HttpPost]
+        public void Post([FromBody] EmployeeModel emp) => context.AddEmployee(emp);
+
+        // PUT api/<EmployeesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] EmployeeModel emp) => context.EditEmployeeById(emp, id);
+
+        // DELETE api/<EmployeesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id) => context.DeleteEmployeeById(id);
+    }
+}
---
+++ EmployeesMicroServices-main/EmployeesMicroServices/EmployeesMicroServices/Models/EmployeeDAL.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.SqlClient;
+
+namespace EmployeesMicroServices.Models
+{
+    public class EmployeeDAL
+    {
+        static SqlConnection cn;
+        static SqlConnection ConnectToSql()
+        {
+            cn = new SqlConnection("server=DESKTOP-DM30GHJ;user id=sa;password=welcome;database=EmpDB");
+            cn.Open();
+            return cn;
+        }
+        public static List<EmployeeModel> GetEmployeesBySql()
+        {
+            List<EmployeeModel> employees = new List<EmployeeModel>();
+            SqlCommand cmd = new SqlCommand("Select * From tblEmployees", ConnectToSql());
+            SqlDataReader dr = cmd.ExecuteReader();
+            EmployeeModel emp;
+            while (dr.Read())
+            {
+                emp = new EmployeeModel();
+                emp.Id = Convert.ToInt32(dr[0]);
+                emp.EName = dr[1].ToString();
+                emp.Job = dr[2].ToString();
+                emp.Salary = Convert.ToInt32(dr[3]);
+                employees.Add(emp);
+            }
+            return employees;
+        }
+    }
+}
